b5f55ee397dff2d0aa9e855a9b6282c0cbc01b34
umeyuki <umeyuki1326+github@gmail.com>
2016-01-18 18:40:30 +0900

    Add user model

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ActiveRecord::Base
+  validates :skype_name,
+            length: {
+              in: 6..32,
+            },
+            format: {
+              with: /\A[a-zA-Z\d][\w:,.-]+\z/,
+              message:  "は正しいSkype名形式で入力して下さい"
+            }
+end
