b32e4bf17c1c9442d2481067557cc9120eaf0380
umeyuki <umeyuki1326+github@gmail.com>
2016-08-12 18:27:38 +0900

    Change a validation of skype name

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
               in: 6..32,
             },
             format: {
-              with: /\A[a-zA-Z\d][\w:,.-]+\z/,
+              with: /\A[a-zA-Z\d][\w,.-]+\z/,
               message:  "は正しいSkype名形式で入力して下さい"
             }
 end
